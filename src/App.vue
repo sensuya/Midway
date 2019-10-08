f30e128bc6fc065a0444b5a30378db79fafb76bf
sinjada <reading.making@gmail.com>
2019-10-08 20:03:40 +0900

    vuetify 적용

--- src/App.vue
+++ src/App.vue
@@ -77,12 +77,6 @@
         </v-layout>
       </v-container>
     </v-content>
-    <v-footer
-      color="indigo"
-      app
-    >
-      <span class="white--text">&copy; 2019</span>
-    </v-footer>
   </v-app>
 </div>
 </template>
