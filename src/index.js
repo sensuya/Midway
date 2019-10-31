6311bd685a8958f9dc6961e4985e3f3709f5183d
sinjada <reading.making@gmail.com>
2019-10-31 19:18:58 +0900

    fortify default layout

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'babel-polyfill'
+import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
@@ -6,10 +7,16 @@ import App from './App'
 
 Vue.use(Vuetify)
 
-const opts = { theme: { dark: true } };
+
+
+const opts = { theme: { 
+                dark: true }, 
+               icons: {
+                iconfont: 'mdi',
+} };
 
 new Vue({
   el: '#app',
-  vuetify: new Vuetify(),
+  vuetify: new Vuetify(opts),
   render: h => h(App)
 })
