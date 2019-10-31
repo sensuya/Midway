817fc4911c944a36d2d659026d37c3579dd5fddf
Ayden <kyubeom23.choi@gmail.com>
2019-10-31 19:46:50 +0900

    apply lint

--- src/component/navigation.vue
+++ src/component/navigation.vue
@@ -1,29 +1,26 @@
 <template>
-      <v-list dense>
-        <v-list-item :key="item.key" v-for="item in listItem">
-          <v-list-item-action>
-            <v-icon>{{item.icon}}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>
-                {{item.title}}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>      
-    </v-list>
+  <v-list dense>
+    <v-list-item :key="item.key" v-for="item in listItem">
+      <v-list-item-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
 export default {
-    name: 'Navigation',
-    data () {
-        return {
-            listItem: [
-                { key: 1, icon : 'home', title: '홈' },
-                { key: 2, icon : 'mdi-tennis', title: '자유게시판' },
-            ]
-        }
-    }
-}
-
+  name: 'Navigation',
+  data() {
+    return {
+      listItem: [
+        { key: 1, icon: 'home', title: '홈' },
+        { key: 2, icon: 'mdi-tennis', title: '자유게시판' },
+      ],
+    };
+  },
+};
 </script>
