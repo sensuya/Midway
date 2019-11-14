7e73d45092ad49e6726741fd18b2947f8ab01692
Ayden <kyubeom23.choi@gmail.com>
2019-11-15 00:55:52 +0900

    vue router init

--- src/App.vue
+++ src/App.vue
@@ -1,117 +1,27 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
-      <v-navigation-drawer v-model="drawer" app>
-        <v-list dense>
-          <v-list-item @click>
-            <v-list-item-action>
-              <v-icon>home</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>홈</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item @click>
-            <v-list-item-action>
-              <v-icon>mdi-tennis</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>자유게시판</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-app-bar app color="indigo" dark>
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>Application</v-toolbar-title>
-      </v-app-bar>
-
-      <v-content>
-        <v-container fluid fill-height>
-          <v-layout align-center justify-center>
-            <v-flex text-xs-center>
-              <v-tooltip left>
-                <template v-slot:activator="{ on }">
-                  <v-btn :href="source" icon large target="_blank" v-on="on">
-                    <v-icon large>mdi-code-tags</v-icon>
-                  </v-btn>
-                </template>
-                <span>Source</span>
-              </v-tooltip>
-
-              <v-tooltip right>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    icon
-                    large
-                    href="https://codepen.io/johnjleider/pen/rJdVMq"
-                    target="_blank"
-                    v-on="on"
-                  >
-                    <v-icon large>mdi-codepen</v-icon>
-                  </v-btn>
-                </template>
-                <span>Codepen</span>
-              </v-tooltip>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-
-      <v-footer dark padless>
-        <v-card flat tile class="indigo lighten-1 white--text text-center">
-          <v-card-text>
-            <v-btn
-              v-for="icon in icons"
-              :key="icon"
-              class="mx-4 white--text"
-              icon
-            >
-              <v-icon size="24px">{{ icon }}</v-icon>
-            </v-btn>
-          </v-card-text>
-
-          <v-card-text class="white--text pt-0">
-            Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
-            Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
-            accumsan id ultrices nunc. Sed at orci sed massa consectetur
-            dignissim a sit amet dui. Duis commodo vitae velit et faucibus.
-            Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum
-            ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel
-            diam elementum tempor vel ut orci. Orci varius natoque penatibus et
-            magnis dis parturient montes, nascetur ridiculus mus.
-          </v-card-text>
-
-          <v-divider></v-divider>
-
-          <v-card-text class="white--text">
-            {{ new Date().getFullYear() }} —
-            <strong>Vuetify</strong>
-          </v-card-text>
-        </v-card>
-      </v-footer>
-    </v-app>
-  </div>
+  <v-app class="d-flex">
+    <div><Header /></div>
+    <Navigation />
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
-import Footer from './component/Footer.vue';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Header from './component/Header.vue';
+import Navigation from './component/Navigation.vue';
+
+Vue.use(VueRouter);
 
 export default {
   name: 'App',
-  components: { Footer },
+  components: { Header, Navigation },
   data() {
     return {
       message: 'Using Parcel In A Vue.js App',
       //drawer: {},
-      icons: [
-        'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
-        'fab fa-instagram',
-      ],
+      icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-google-plus', 'fab fa-linkedin', 'fab fa-instagram'],
     };
   },
 };
---
+++ src/component/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar :class="`d-flex justify-center align-center`" height="100rem" style="padding-top: 1.5rem;">
+    <p style="padding-right:10px;">흥정망정</p>
+    <v-text-field rounded="true" outlined="true"></v-text-field>
+    <v-btn icon class="test"><v-icon>mdi-magnify</v-icon></v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
+
+<style>
+.test {
+  display: inline;
+}
+</style>
