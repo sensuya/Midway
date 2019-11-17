1a9437e1a0ca10480f10d6cd1c0115df0f29482a
juhee.seon <juheeseon@gmail.com>
2019-11-17 23:42:46 +0900

    라우터의 path와 매핑되는 component는 vue 파일로 바꾸니깐 된다

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import 'babel-polyfill';
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import Vuetify from 'vuetify';
-import routes from './route/router';
+import router from './route';
 import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App';
 
 Vue.use(Vuetify);
-Vue.use(VueRouter);
-
-const router = new VueRouter({ routes });
 
 const opts = {
   icons: {
@@ -20,8 +16,8 @@ const opts = {
 };
 
 new Vue({
-  router,
   el: '#app',
   vuetify: new Vuetify(opts),
   render: h => h(App),
+  router,
 });
