feae0f38476ec34555133831664fbfaae4b00bc7
sinjada <reading.making@gmail.com>
2019-11-27 20:21:53 +0900

    table에 prop작업 완료 홈에다가 table 컴포넌트 추가

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,13 +1,53 @@
 <template>
-  <div>Home</div>
+  <div>
+    Home
+    <div>
+      <Table :items="items" :headers="headers" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Table from '../component/Table';
 
-}
+export default {
+  components: { Table },
+  data: () => {
+    return {
+      headers: [
+        {
+          text: '번호',
+          align: 'center',
+          value: 'no',
+          width: '10%',
+        },
+        { text: '제목', value: 'title', align: 'left', width: '50%' },
+        { text: '글쓴이', value: 'writer', align: 'center', width: '10%' },
+        { text: '작성일', value: 'create_date', align: 'center', width: '10%' },
+        { text: '조회', value: 'click_no', align: 'center', width: '10%' },
+        { text: '추천', value: 'recomend_no', align: 'center', width: '10%' },
+      ],
+      items: [
+        {
+          no: '5123645',
+          title: '현진이 요즘 빵 안먹나',
+          writer: '창피해',
+          create_date: '19.07.25',
+          click_no: '4',
+          recomend_no: '1',
+        },
+        {
+          no: '5123644',
+          title: '최규범 선주희',
+          writer: '최선',
+          create_date: '19.07.26',
+          click_no: '5',
+          recomend_no: '3',
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
